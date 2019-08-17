9cb3e6b33168f3c394f3bb61ffcf4ac494efc594
Shen Ziming <2508754153@qq.com>
2019-08-17 12:54:21 +0800

    fix: exceptions in row transition and determinant
    1. Add lower triangle transition feature.
    2. Fix bugs when text starts with a few '\n's or lines starts with a few ' 's.
    3. Fix bugs when the number of rows neq the number of columns in row
    transition.
    4. Fix bugs about changing the sign of the determinant when there is no
    any two rows interchanged.
    5. Fix bugs when there's a row with all zero in row transition process
    of determinant calculating.
    6. Fix bugs when delete action fails when click the 'cross' icon.

--- app/src/main/java/com/example/shen/linearassist/DeterminantHome.java
+++ app/src/main/java/com/example/shen/linearassist/DeterminantHome.java
@@ -46,8 +46,16 @@ public class DeterminantHome extends AppCompatActivity {
             if (!isNumerical(determinantText)) { // 非纯数字异常
                 throw new NonNumericalException();
             }
+            while (determinantText.charAt(0) == '\n') {
+                determinantText = determinantText.substring(1);
+            }
             String[] temp1 = determinantText.split("\\n+"); // 行列式文本按行截取
             int order = temp1.length;
+            for (int i = 0; i < order; i++) {
+                while (temp1[i].charAt(0) == ' ') {
+                    temp1[i] = temp1[i].substring(1);
+                }
+            }
             for (int i = 0; i < order; i++) {
                 if (temp1[i].split("\\s+").length != order) {
                     throw new NonSquareMatrixException("第" + i + "行的列数与行数不符");
@@ -70,20 +78,24 @@ public class DeterminantHome extends AppCompatActivity {
             /* 行初等变换 */
             while (pOrder < order - 1) {
                 /* minRow为除去已经变换后的行之外， 从左到右首个非零元最小的行索引*/
+                minRow = pOrder;
                 double tempMin = determinant[pOrder][pOrder];
-                for (int i = pOrder; i < order - 1; i++) {
-                    for (int j = i + 1; j < order; j++) {
-                        if (determinant[j][pOrder] < tempMin && determinant[j][pOrder] != 0){
-                            tempMin = determinant[j][pOrder];
-                            minRow = j;
-                        }
+                for (int i = pOrder + 1; i < order; i++) {
+                    if (determinant[i][pOrder] < tempMin && determinant[i][pOrder] != 0) {
+                        tempMin = determinant[i][pOrder];
+                        minRow = i;
                     }
                 }
+                if (determinant[minRow][pOrder] == 0) {
+                    break;
+                }
                 /* minRow行由于有最小的首个非零元，将其换到剩余行的顶部有利于行变换时减少小数出现 */
-                double[] tempRow = determinant[minRow];
-                determinant[minRow] = determinant[pOrder];
-                determinant[pOrder] = tempRow;
-                resultD = - resultD; // 第一类行变换变号
+                if (minRow != pOrder) {
+                    double[] tempRow = determinant[minRow];
+                    determinant[minRow] = determinant[pOrder];
+                    determinant[pOrder] = tempRow;
+                    resultD = -resultD; // 第一类行变换变号
+                }
                 for (int i = pOrder + 1; i < order; i++) {
                     double div = bigDecimalDiv(determinant[i][pOrder], determinant[pOrder][pOrder]); // 对下列行实施第三类行变换的乘数因子
                     for (int j = pOrder; j < order; j++) { // 对下面的行实施第三类行变换，以求上三角阵
--- app/src/main/java/com/example/shen/linearassist/RowTransitionHome.java
+++ app/src/main/java/com/example/shen/linearassist/RowTransitionHome.java
@@ -26,6 +26,7 @@ public class RowTransitionHome extends AppCompatActivity {
 
     private static final int ACCURACY = 4;
     Button upper_triangle_transition_button;
+    Button lower_triangle_transition_button;
     EditTextWithDel matrix_content;
 
     @Override
@@ -33,25 +34,39 @@ public class RowTransitionHome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_row_transition_home);
         upper_triangle_transition_button = (Button) findViewById(R.id.upper_triangle_transition_button);
+        lower_triangle_transition_button = (Button) findViewById(R.id.lower_triangle_transition_button);
         matrix_content = (EditTextWithDel) findViewById(R.id.matrix_context);
         upper_triangle_transition_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                rowTransition();
+                rowTransition(true);
+            }
+        });
+        lower_triangle_transition_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rowTransition(false);
             }
         });
     }
 
-    private void rowTransition() {
+    private void rowTransition(boolean isUpper) {
         String result = "";
         try {
             String matrixContent = matrix_content.getText().toString(); // 获取行列式信息
-
             if (!isNumerical(matrixContent)) { // 非纯数字异常
                 throw new NonNumericalException();
             }
+            while (matrixContent.charAt(0) == '\n') {
+                matrixContent = matrixContent.substring(1);
+            }
             String[] temp1 = matrixContent.split("\\n+"); // 行列式文本按行截取
             int rowN = temp1.length;
+            for (int i = 0; i < rowN; i++) {
+                while (temp1[i].charAt(0) == ' ') {
+                    temp1[i] = temp1[i].substring(1);
+                }
+            }
             for (int i = 0; i < rowN - 1; i++) {
                 if (temp1[i].split("\\s+").length != temp1[i + 1].split("\\s+").length) {
                     throw new NonSquareMatrixException("第" + i + "行的列数与其他行行数不符");
@@ -66,44 +81,77 @@ public class RowTransitionHome extends AppCompatActivity {
             for (int i = 0; i < rowN; i++) {
                 temp2[i] = temp1[i].split("\\s+");
             }
-            Log.d("rowN= ", String.valueOf(rowN));
-            Log.d("columnN= ", String.valueOf(columnN));
             for (int i = 0; i < rowN; i++) { // 获取行列式二维数值数组
                 for (int j = 0; j < columnN; j++) {
                     determinant[i][j] = Double.parseDouble(temp2[i][j]);
                 }
             }
-            int minRow = 0;
-            int pOrder = 0;
-            /* 行初等变换 */
-            int upperOrder = rowN > columnN ? columnN : rowN;
-            while (pOrder < upperOrder - 1) {
-                /* minRow为除去已经变换后的行之外， 从左到右首个非零元最小的行索引*/
-                double tempMin = determinant[pOrder][pOrder];
-                for (int i = pOrder; i < rowN - 1; i++) {
-                    for (int j = i + 1; j < columnN; j++) {
-                        if (determinant[j][pOrder] < tempMin && determinant[j][pOrder] != 0) {
-                            tempMin = determinant[j][pOrder];
-                            minRow = j;
+            if (isUpper) { // 上三角行变换
+                int pOrder = 0;
+                int minRow = 0;
+                /* 行初等变换 */
+                int upperOrder = rowN > columnN ? columnN : rowN - 1;
+                while (pOrder < upperOrder) {
+                    /* minRow为除去已经变换后的行之外， 从左到右首个非零元最小的行索引*/
+                    minRow = pOrder;
+                    double tempMin = determinant[pOrder][pOrder];
+                    for (int i = pOrder + 1; i < rowN; i++) {
+                        if (determinant[i][pOrder] < tempMin && determinant[i][pOrder] != 0) {
+                            tempMin = determinant[i][pOrder];
+                            minRow = i;
                         }
                     }
-                }
-                /* minRow行由于有最小的首个非零元，将其换到剩余行的顶部有利于行变换时减少小数出现 */
-                double[] tempRow = determinant[minRow];
-                determinant[minRow] = determinant[pOrder];
-                determinant[pOrder] = tempRow;
-                for (int i = pOrder + 1; i < rowN; i++) {
-                    double div = bigDecimalDiv(determinant[i][pOrder], determinant[pOrder][pOrder]); // 对下列行实施第三类行变换的乘数因子
-                    Log.d(i + "," + pOrder, String.valueOf(determinant[i][pOrder]));
-                    Log.d(pOrder + "," + pOrder, String.valueOf(determinant[pOrder][pOrder]));
-                    Log.d("div= ", String.valueOf(div));
-                    for (int j = pOrder; j < columnN; j++) { // 对下面的行实施第三类行变换，以求上三角阵
-                        determinant[i][j] = bigDecimalSub(determinant[i][j], bigDecimalMul(determinant[pOrder][j], div));
-                        Log.d(String.valueOf(pOrder), i + "," + j + ": " + determinant[i][j]);
+                    /* minRow行由于有最小的首个非零元，将其换到剩余行的顶部有利于行变换时减少小数出现 */
+                    if (determinant[minRow][pOrder] == 0) {
+                        break;
                     }
+                    if (minRow != pOrder) {
+                        double[] tempRow = determinant[minRow];
+                        determinant[minRow] = determinant[pOrder];
+                        determinant[pOrder] = tempRow;
+                    }
+                    for (int i = pOrder + 1; i < rowN; i++) {
+                        double div = bigDecimalDiv(determinant[i][pOrder], determinant[pOrder][pOrder]); // 对下列行实施第三类行变换的乘数因子
+                        for (int j = 0; j < columnN; j++) { // 对下面的行实施第三类行变换，以求上三角阵
+                            determinant[i][j] = bigDecimalSub(determinant[i][j], bigDecimalMul(determinant[pOrder][j], div));
+                        }
+                    }
+                    pOrder++;
+                } // END 行初等变换
+            } else {
+                int iOrder = rowN - 1;
+                int jOrder = columnN - 1;
+                int minRow = 0;
+                while (iOrder > 0 && jOrder >= 0) {
+                    /* minRow为除去已经变换后的行之外， 从左到右首个非零元最小的行索引*/
+                    minRow = iOrder;
+                    double tempMin = determinant[iOrder][jOrder];
+                    for (int i = iOrder - 1; i >= 0; i--) {
+                        if (determinant[i][jOrder] < tempMin && determinant[i][jOrder] != 0) {
+                            tempMin = determinant[i][jOrder];
+                            minRow = i;
+                        }
+                    }
+                    /* minRow行由于有最小的首个非零元，将其换到剩余行的顶部有利于行变换时减少小数出现 */
+                    if (determinant[minRow][jOrder] == 0) {
+                        break;
+                    }
+                    if (minRow != iOrder) {
+                        double[] tempRow = determinant[minRow];
+                        determinant[minRow] = determinant[iOrder];
+                        determinant[iOrder] = tempRow;
+                    }
+                    for (int i = iOrder - 1; i >= 0; i--) {
+                        double div = bigDecimalDiv(determinant[i][jOrder], determinant[iOrder][jOrder]);
+                        for (int j = columnN - 1; j >= 0; j--) {
+                            determinant[i][j] = bigDecimalSub(determinant[i][j], bigDecimalMul(determinant[iOrder][j], div));
+                        }
+                    }
+                    iOrder--;
+                    jOrder--;
                 }
-                pOrder++;
-            } // END 行初等变换
+            }
+
 
             for (int i = 0; i < rowN; i++) {
                 for (int j = 0; j < columnN; j++) {
@@ -112,11 +160,19 @@ public class RowTransitionHome extends AppCompatActivity {
                 }
                 result += "\n";
             }
-        } catch (NonNumericalException e) {
+        } catch (
+                NonNumericalException e)
+
+        {
             result = "行列式必须是纯数字！";
-        } catch (NonSquareMatrixException e) {
+        } catch (
+                NonSquareMatrixException e)
+
+        {
             result = e.getMessage();
-        } finally {
+        } finally
+
+        {
             final AlertDialog.Builder normalDialog = new AlertDialog.Builder(RowTransitionHome.this);
             normalDialog.setTitle("结果");
             normalDialog.setMessage(result);
@@ -141,6 +197,7 @@ public class RowTransitionHome extends AppCompatActivity {
             });
             normalDialog.create().show();
         }
+
     }
 
     private boolean copy(String copyStr) {
